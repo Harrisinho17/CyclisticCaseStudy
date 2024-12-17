34eba67b8830b00e8f599992f5ffe3e6f32ae0f1
Alexander B Harris <ABelchior.Harris@gmail.com>
2024-12-16 22:22:13 -0500

    station data and queries.

--- SQL Scripts/Visual Scripts/Station_instances.sql
+++ SQL Scripts/Visual Scripts/Station_instances.sql
@@ -1,6 +1,6 @@
 -- In this script I used instances and the avg latitude and longitude to map the highest instances of start and end stations. 
 
-
+-- Start station details with averqages
 SELECT 
     start_station_name,
     s_station_uid,
@@ -15,9 +15,25 @@ GROUP BY
 ORDER BY 
     instances DESC;
 
+-- Start station details without averages
+SELECT 
+    start_station_name,
+    s_station_uid,
+  	start_lat,
+	start_lng,
+    COUNT(ride_id) AS instances
+FROM 
+    cyclistic_ytd
+GROUP BY 
+    start_station_name, 
+    s_station_uid,
+	start_lat,
+	start_lng
+ORDER BY 
+    instances DESC;
 
 
-
+-- End station details with averages
 SELECT 
     end_station_name,
     e_station_uid,
@@ -29,5 +45,23 @@ FROM
 GROUP BY 
     end_station_name, 
     e_station_uid
+ORDER BY 
+    instances DESC;
+
+
+-- End Station details without averages
+    SELECT 
+    end_station_name,
+    e_station_uid,
+  	end_lat,
+	end_lng,
+    COUNT(ride_id) AS instances
+FROM 
+    cyclistic_ytd
+GROUP BY 
+    endt_station_name, 
+    e_station_uid,
+	end_lat,
+	end_lng
 ORDER BY 
     instances DESC;
