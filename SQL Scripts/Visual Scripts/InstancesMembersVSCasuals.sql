69b679b64c20e0adc1b0108601c2cc0033a41638
Alexander B Harris <ABelchior.Harris@gmail.com>
2024-12-19 17:08:15 -0500

    Updated Member V casual data.

--- SQL Scripts/Visual Scripts/InstancesMembersVSCasuals.sql
+++ SQL Scripts/Visual Scripts/InstancesMembersVSCasuals.sql
@@ -1,10 +1,10 @@
 -- I used this script to get the number of distinct rides members and casuals used the equipment. 
 SELECT 
-	member_casual,
-	rideable_type,
-	COUNT(ride_id) as instances
+    COALESCE(member_casual, 'total') AS member_casual,
+    COUNT(ride_id) AS instances
 FROM 
-	cyclistic_ytd
+    cyclistic_ytd
 GROUP BY 
-	member_casual;
-	rideable_type;
+    ROLLUP(member_casual)
+ORDER BY 
+    instances DESC;
